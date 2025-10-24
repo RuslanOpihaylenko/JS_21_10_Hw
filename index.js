c1d66cf46a58dcec02d1eb276994f3bb64dee2ca
Ruslan Opihaylenko <rysya08shkolnik@gmail.com>
2025-10-24 12:49:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// 1
+// let name  = prompt("Enter name: ");
+// if (name!=null){
+//     alert("Hello "+ name);
+// }
+//2
+// let year_of_birth = prompt("Enter year: ");
+// year_of_birth  = Number(year_of_birth);
+// const year = 2025;
+// if(year_of_birth){
+//     alert("Year: "+(year-year_of_birth));
+// }
+//3
+// let side = prompt("Enter side: ");
+// side  = Number(side);
+// if(side){
+//     alert("P: " + (side*4));
+// }
+//4
+// let radious = prompt("Enter radious: ");
+// radious  = Number(radious);
+// if(radious){
+//     alert("S: " + (2*3.14*radious));
+// }
+//5
+// let distance = prompt("Enter distance: ");
+// let hours = prompt("Enter hours: ");
+// distance  = Number(distance);
+// hours  = Number(hours);
+// if(distance && hours){
+//     alert("Speed is have to be: " + (distance/hours) + " "+ "km/hour");
+// }
+//6
+// let dollar = prompt("Enter dollars: ");
+// dollar  = Number(dollar);
+// const euro = 0.86;
+// if(dollar){
+//     alert("For " + dollar + " dollars you get "+ (dollar*euro) + " euros");
+// }
+//6
+// let size  = prompt("Enter size in gigabytres: ");
+// size = Number(size);
+// const gigabyte = size*1000;
+// const size_in_megabytes = 820;
+// if(size){
+//     alert("You have " + (gigabyte/size_in_megabytes) + " in amount");
+// }
